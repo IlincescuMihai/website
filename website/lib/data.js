f7d516ebd9687cccbc767e8099d4f742bd701363
Ilincescu Mihai <mihai@ilincescu.ro>
2016-07-25 15:39:22 +0300

    A new start

--- website/lib/data.js
+++
@@ -1,8 +0,0 @@
-var data = { "count": 4,
-    "locations": [
-        { "longitude": 24.13, "latitude": 45.79 }
-        ,{ "longitude": 23.13, "latitude": 45.79 }
-        , { "longitude": 14.13, "latitude": 45.79 }
-        , { "longitude": 14.03, "latitude": 45.79 }
-]}
-
